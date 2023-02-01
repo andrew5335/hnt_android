3dbc466b1f0c61be347d428637c2f3769361f892
Andrew <kim.hyoungju@kt.com>
2023-02-01 18:21:15 +0900

    네이티브 화면 개발 진행 중

--- app/src/main/java/com/hnt/hnt_android/IntroActivity.java
+++ app/src/main/java/com/hnt/hnt_android/IntroActivity.java
@@ -2,7 +2,10 @@ package com.hnt.hnt_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // 인트로 화면에서 2초 후 로그인 화면으로 이동
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
